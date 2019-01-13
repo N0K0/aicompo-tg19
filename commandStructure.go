e097512e40537d87b7302e18cb50e67f47c390d9
Nikolas Papaioannou <nikolasp@ifi.uio.no>
2019-01-13 20:23:26 +0100

    Add tag for json

--- commandStructure.go
+++ commandStructure.go
@@ -2,10 +2,10 @@ package main
 
 // Command struct defines how commands from players/admin should be formatted
 type Command struct {
-	command string
+	Command string `json:"command"`
 }
 
-// Structure defining gamestatus to the players
+// Structure defining game status to the players
 type gameStatus struct {
 	updateType string
 	data       string
