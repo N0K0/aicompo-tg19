50c16f91ed0fb5111bfb327d78157a02524624b6
Nikolas Papaioannou <nikolasp@ifi.uio.no>
2019-01-20 15:51:42 +0100

    Rework commandstruct into one

--- commandStructure.go
+++ commandStructure.go
@@ -2,11 +2,6 @@ package main
 
 // Command struct defines how commands from players/admin should be formatted
 type Command struct {
-	Command string `json:"command"`
-}
-
-// Structure defining game status to the players
-type gameStatus struct {
-	updateType string
-	data       string
+	CommandType string `json:"type"`
+	Command     string `json:"command"`
 }
